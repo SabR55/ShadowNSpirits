f0ec0e67cb945c259e5a36bcf51a420b235bd381
SabR55 <sabila.rasyida@gmail.com>
2025-03-07 18:50:22 +0800

    Database can successfully connect

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = 3000;
 const app = express();
 
 app.use(express.json());
@@ -8,6 +12,18 @@ app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
 }));
+
+// MongoDB Connection
+// afij3bSEAY8TSQyo
+const url = process.env.MONGODB_URL;
+
+console.log(url);
+
+
+mongoose.connect(url, {})
+.then(result => console.log("Database is connected"))
+.catch(err => console.log(err))
+
 
 app.get('/test', (req,res) => {
     res.json('test ok');
@@ -19,4 +35,6 @@ app.post('/makeReservation', (req,res) => {
     res.json({formData});
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log("Server is running at port " + port)
+})
