eda2a33ede05ff28a53724b777a3fc12b0e7b69b
SabR55 <sabila.rasyida@gmail.com>
2025-04-09 20:52:59 +0800

    Update assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,15 +1,15 @@
-import homeHero from './images/home-heroImage.jpg';
-import homeCard01 from './images/home-card1.jpg';
-import homeCard02 from './images/home-card2.jpg';
-import homeCard03 from './images/home-card3.jpg';
-import homeDecor from './images/home-decor.png';
-import smokeBg from './images/smoke-bgd.jpg';
-import homeImg01 from './images/home-image1.jpg';
-import homeImg02 from './images/home-image2.jpg';
-import ourDrinksHero from './images/ourDrinks-hero.jpg';
-import ourDrinksCard01 from './images/ourDrinks-card1.jpg';
-import ourDrinksCard02 from './images/ourDrinks-card2.jpg';
-import ourDrinksCard03 from './images/ourDrinks-card3.jpg';
+import homeHero from './Images/home-heroImage.jpg';
+import homeCard01 from './Images/home-card1.jpg';
+import homeCard02 from './Images/home-card2.jpg';
+import homeCard03 from './Images/home-card3.jpg';
+import homeDecor from './Images/home-decor.png';
+import smokeBg from './Images/smoke-bgd.jpg';
+import homeImg01 from './Images/home-image1.jpg';
+import homeImg02 from './Images/home-image2.jpg';
+import ourDrinksHero from './Images/ourDrinks-hero.jpg';
+import ourDrinksCard01 from './Images/ourDrinks-card1.jpg';
+import ourDrinksCard02 from './Images/ourDrinks-card2.jpg';
+import ourDrinksCard03 from './Images/ourDrinks-card3.jpg';
 
 
 const images = {
